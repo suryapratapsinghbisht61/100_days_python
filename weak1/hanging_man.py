2aefe4eafa92b0103a503bd7a2e1234be6bfa56f
Surya Pratap Singh <suryapratapsinghbisht61@gmail.com>
2026-04-08 20:19:37 +0530

    hangman done

--- weak1/hanging_man.py
+++ weak1/hanging_man.py
@@ -4,26 +4,34 @@ chosen_word=random.choice(word_list)
 x=""
 
 for i in range(len(chosen_word)):
-    x+="_ "
+    x+="_"
 print(x)
 
 game_end=False
 
+live=6
 hh=[]
 while not game_end:
-    dis=''
+    dis=''  
     guess=input("guess the word :  ").lower()
     for i in chosen_word:
-        if guess == i:
+        if i == guess:
             dis+=i
-            hh.append(guess)
+            hh.append(i)
         
         elif i in hh:
-            dis+="_ "
+            dis+=i
         
         else:
-            dis+="_ "
+            dis+="_"
     print(dis)
-    if "_ " not in dis:
+    if "_" not in dis:
         game_end=True
         print("you win")
+        
+if live not in guess:
+    live-=1
+    if live ==0:
+        game_end=True
+        print("you lose")
+        

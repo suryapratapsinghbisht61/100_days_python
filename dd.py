35471ba6760b2fb644ab640046e534f1c6193290
Surya Pratap Singh <suryapratapsinghbisht61@gmail.com>
2026-04-10 16:37:57 +0530

    keywork argument

--- dd.py
+++ dd.py
@@ -1,8 +1,16 @@
-def howis(name,location):
+"""def howis(name,location):
     print(f"hello {name}")
     print(f"how is it in {location}")
     
 x=input("enter your name :  ")
 y=input("enter your city :  ")
 howis(x,y)
+    """
     
+#usig key work argument
+def c(n,l):
+     print(f"hello {n}")
+     print(f"how is it in {l}")
+    
+
+c(l="almora",n="surya")

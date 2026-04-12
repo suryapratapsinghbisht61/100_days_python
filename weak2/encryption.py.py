9f0019114140d52557181642544b9f36e8d7fa38
Surya Pratap Singh <suryapratapsinghbisht61@gmail.com>
2026-04-12 18:41:53 +0530

    caesar cipher

---
+++ weak2/encryption.py.py
@@ -0,0 +1,29 @@
+alphabet=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction1=input("enter 'encode' to encrypt 'decode' for decript:\n").lower()
+text=input("enter the message: \n ").lower()
+shift=int(input("type the number: \n"))
+
+def caesar(direction):
+    if direction=="encode":   
+        def encrypt(original_text,shift_amount):
+            
+            text="" 
+            for i in original_text:
+                    shift_posetion=alphabet.index(i)+shift_amount
+                    shift_posetion%=len(alphabet)
+                    text+=alphabet[shift_posetion]
+            print(f"here is the encoded result {text}")       
+        encrypt(original_text=text , shift_amount=shift)
+    elif direction=="decode":      
+        def decrypt(original_text,shift_amount):
+            text=""
+            for i in original_text:
+                    shift_posetion=alphabet.index(i)-shift_amount
+                    shift_posetion%=len(alphabet)
+                    text+=alphabet[shift_posetion]
+            print(f"here is the decrypted result {text}")
+        decrypt(original_text=text,shift_amount=shift)
+    else:
+        print("enter encode or decode first")
+caesar(direction=direction1)
